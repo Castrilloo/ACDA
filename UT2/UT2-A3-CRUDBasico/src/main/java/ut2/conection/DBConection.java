a7f072c3d2fdb275da2e00ee79a854b1fdf672de
Castrilloo <castrollos2@gmail.com>
2025-10-16 11:33:19 +0200

    ejercicios dentro

---
+++ UT2/UT2-A3-CRUDBasico/src/main/java/ut2/conection/DBConection.java
@@ -0,0 +1,43 @@
+package ut2.conection;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class DBConection {
+
+    private static final Logger logger = Logger.getLogger(DBConection.class.getName());
+
+    private static DBConection instancia;
+
+    private Connection conexion;
+
+    private final String PUERTO = "3308";
+    private final String ESQUEMA = "acda_ut2";
+    private final String SGBD = "mysql";
+    private final String url = "jdbc:"+SGBD +"://localhost:" + PUERTO +"/"+ESQUEMA;
+    private final String usuario = "root";
+    private final String contrasena= "root";
+
+
+    private DBConection(){
+        try{
+            conexion = DriverManager.getConnection(url,usuario,contrasena);
+            logger.info("conexion exitosa a la base de datos");
+        }catch (SQLException e){
+            logger.severe("Error al conectar a la base de datos: "+ e.getMessage());
+        }
+    }
+
+    public static DBConection getInstancia(){
+        if (instancia==null){
+            instancia = new DBConection();
+        }
+        return instancia;
+    }
+
+    public Connection getConexion(){
+        return conexion;
+    }
+}
